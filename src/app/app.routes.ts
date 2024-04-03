8e7d46e2f4200208b08cc6675125fa1738b0cb23
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2024-04-03 09:14:36 +0600

    Trainer Table

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { GymCardsComponent } from './components/gym-cards/gym-cards.component';
 import { TrainerListComponent } from './components/trainer-list/trainer-list.component';
+import { TrainerTableComponent } from './components/trainer-table/trainer-table.component';
 
 export const routes: Routes = [
     {path:'',component:GymCardsComponent},
     {
         path: 'gym/:id', component:TrainerListComponent
+    },
+    {
+        path: 'trainer/:id', component: TrainerTableComponent
     }
 ];
---
+++ src/app/components/trainer-table/trainer-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trainer-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './trainer-table.component.html',
+  styleUrl: './trainer-table.component.css'
+})
+export class TrainerTableComponent {
+
+}
