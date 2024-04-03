ce663a33fb2063d76cd7c3ea95647d08bd880a70
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2024-04-03 08:58:05 +0600

    Trainer Components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { GymCardsComponent } from './components/gym-cards/gym-cards.component';
+import { TrainerListComponent } from './components/trainer-list/trainer-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:GymCardsComponent},
+    {
+        path: 'gym/:id', component:TrainerListComponent
+    }
+];
---
+++ src/app/components/trainer-list/trainer-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trainer-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './trainer-list.component.html',
+  styleUrl: './trainer-list.component.css'
+})
+export class TrainerListComponent {
+
+}
