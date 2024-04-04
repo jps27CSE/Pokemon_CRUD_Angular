169444c81d9f677e59ea81afe4289a14b6a4bed7
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2024-04-04 12:47:06 +0600

    Aos Animation Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { GymCardsComponent } from './components/gym-cards/gym-cards.component';
@@ -6,6 +6,8 @@ import { FooterComponent } from './components/footer/footer.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 @Component({
   selector: 'app-root',
@@ -21,6 +23,13 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Pokemon_Crud_Angular';
+
+  ngOnInit() {
+    AOS.init({
+      duration: 1000, // Duration of animation in milliseconds
+      once: true, // Animation will only happen once
+    });
+  }
 }
